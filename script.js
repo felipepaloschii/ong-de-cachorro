b2d6335716dd2384bc424034e46140b4d8d0d352
felipe paloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-04-03 12:12:07 -0300

    add - terminei index e começei java

---
+++ script.js
@@ -0,0 +1,10 @@
+function formulario() {
+
+    let nome = document.getElementById("nome").value;
+        let email = document.getElementById("email").value;
+                let telefone = document.getElementById("telefone").value;
+                        let cpf = document.getElementById("cpf").value;
+
+
+
+}
